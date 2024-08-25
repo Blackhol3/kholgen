da8f87b9362bd91d5623aed525755672423bd38f
Blackhol3 <amaury.dallamonta@gmail.com>
2024-08-25 21:25:47 +0200

    Improve packaging.

--- package.sh
+++ package.sh
@@ -12,21 +12,11 @@ if [ -z "$SOLVER_BUILD_PATH" ]; then
 	exit
 fi
 
-if [ -d package/ ]; then
-	echo "The whole ${PWD}/package folder will be emptied. Continue?"
-	select yn in "Yes" "No"; do
-		case $yn in
-			Yes) break;;
-			No) exit;;
-		esac
-	done
-fi
-
 rm -rf package/
 mkdir package/
 cd package/
 
-cp -r "../${SOLVER_BUILD_PATH}/package" solver/
-cp -r "../user-interface/browser" user-interface/
-cp "../user-interface/3rdpartylicenses.txt" user-interface/
+cp -rp "../${SOLVER_BUILD_PATH}/package" solver/
+cp -rp "../user-interface/browser" user-interface/
+cp -p "../user-interface/3rdpartylicenses.txt" user-interface/
 create-shortcut "${PWD}/solver/KholGen.exe" "${PWD}/KholGen.lnk"
